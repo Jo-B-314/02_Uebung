3835fe6d939bc304cb5234d4a1e71a883633893f
Lisa-Marie Rolli <s8liroll@stud.uni-saarland.de>
2020-11-24 11:24:54 +0100

    build deleted an images changed

--- Uebung02/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stud/s8jobech/2.Uebung/Uebung02
-# Build directory: /home/stud/s8jobech/2.Uebung/Uebung02/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(test)
